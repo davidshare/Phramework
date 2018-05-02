6e80a1e3df9b881f5baf5206a7b2be140c938a96
David Essien <davidessienshare@gmail.com>
2018-05-02 02:25:18 +0100

    take index method to the top

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,12 +4,12 @@
 			
 		}
 
-		public function about($id){
-			echo $id;
+		public function index(){
+			$this->view('hello');
 		}
 
-		public function index(){
-
+		public function about($id){
+			echo $id;
 		}
 	}
 ?>
