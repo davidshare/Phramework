a0b4ee9e3281b0b15bbf0fa25d9dfdd7a761db34
David Essien <davidessienshare@gmail.com>
2018-05-02 03:26:42 +0100

    remove call to libraries

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,12 +1,7 @@
 <?php
 	//load Config
 	require_once 'config/config.php';
-
-	//require Libraries
-	require_once('libraries/core.php');
-	require_once('libraries/controller.php');
-	require_once('libraries/database.php');
-
+	
 	//Autoload core libraries
 	spl_autoload_register(function($className){
 		require_once 'libraries/'.$className.'.php';
