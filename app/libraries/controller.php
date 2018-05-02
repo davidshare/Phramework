8b635f7cd11fe2df86ca0b75b42ae2778c31e876
David Essien <davidessienshare@gmail.com>
2018-05-02 02:34:30 +0100

    fix file path bug

--- app/libraries/controller.php
+++ app/libraries/controller.php
@@ -23,7 +23,7 @@
 		public function view($view, $data = []){
 			//check for the view file
 			if(file_exists('../app/views/'.$view.'.php')){
-				require_once '../app/views'.$view.'.php';
+				require_once '../app/views/'.$view.'.php';
 			}else{
 				//view does not exists
 				die('view does not exist');
