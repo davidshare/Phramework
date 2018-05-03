953e8ac8725a72739311110e7cbcc484abab2401
David Essien <davidessienshare@gmail.com>
2018-05-03 11:58:53 +0100

    clean out post specifict content

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -1,11 +1,13 @@
 <?php
 	class Pages extends Controller{
 		public function __construct(){
-			$this->postModel = $this->model('Post');
+			
 		}
 
 		public function index(){
-			$data = ['title'=>'Homepage'];
+			$data = [
+				'title'=>'Homepage'
+			];
 			$this->view('pages/index', $data);
 		}
 
