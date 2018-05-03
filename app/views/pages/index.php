94d48c2edf94393569b33ce6f137fa2bbd241739
David Essien <davidessienshare@gmail.com>
2018-05-03 12:46:41 +0100

    add tagline to homepage title

--- app/views/pages/index.php
+++ app/views/pages/index.php
@@ -2,6 +2,7 @@
 	require APPROOT.'/views/inc/header.php';
 ?>
 <h1><?php echo $data['title']; ?></h1>
+<p>Welcome to Phramework a simple MVC framework built with php </p>
 <?php
 	require APPROOT.'/views/inc/footer.php';
 ?>
