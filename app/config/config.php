c4b1ae9d395cbd7a448f52bb3d4c2b5eaa5917d3
David Essien <davidessienshare@gmail.com>
2018-05-03 12:00:11 +0100

    replace spefic db information with dummy data

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php 
 
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', 'gemshare,php1989@');
-	define('DB_NAME', 'phramework');
+	define('DB_USER', '_YOUR_USER_');
+	define('DB_PASS', '_YOUR_PASS_');
+	define('DB_NAME', '_YOUR_DB_NAME_');
 	//App Root
 	define('APPROOT', dirname(dirname(__FILE__)));
 
